782c5bf6b2160365dac5b878a631064becc2d21e
JosefineTM <137281546+JosefineTM@users.noreply.github.com>
2024-11-07 13:39:26 +0100

    Delete streamlit_report/sections/combined directory

--- streamlit_report/sections/combined/combined_conclusion.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-import json
-
-st.markdown('''<h3 style='text-align: center; color: #023558;'>Conclusion</h3>''', unsafe_allow_html=True)
-st.markdown('''<p style='text-align: center; color: #000000;'>Conclusion of thesis</p>''', unsafe_allow_html=True)
-st.markdown('''<h4 style='text-align: center; color: #2b8cbe;'>Kaplan Meier plot of discovery cohort</h4>''', unsafe_allow_html=True)
-
-st.image('example_data/DemProt/kaplan_meier_plot.png', caption='Kaplan Meier survival plot for the whole discovery cohort over time.', use_column_width=True)
